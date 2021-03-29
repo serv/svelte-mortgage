8ed86ce3e261efa29288ec030913b4e54f83ff38
Jason Kim <iamjsonkim@gmail.com>
2021-03-29 09:11:58 -0700

    Button

---
+++ src/components/Button/Button.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  export let options;
+  export let content;
+  export let color;
+  export let isDiv;
+  color = color ? color : 'blue';
+
+  let className = `max-w-max bg-${color}-600 text-white rounded-lg px-4 py-2 border-${color}-800 hover:bg-${color}-700 focus:outline-none focus:ring-2 focus:ring-${color}-700 focus:ring-opacity-50`;
+</script>
+
+{#if isDiv}
+  <div class={className} {...options}>{content}</div>
+{:else}
+  <button type="button" class={className} {...options}>{content}</button>
+{/if}
