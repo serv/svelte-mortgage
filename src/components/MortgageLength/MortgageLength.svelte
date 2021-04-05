f3e14a49ad352d0265ea86e9e68c5ac568fda0c8
Jason Kim <iamjsonkim@gmail.com>
2021-04-05 13:55:45 -0700

    Mortgage Length (#10)
    * ui is good
    
    * Mortgage Length

---
+++ src/components/MortgageLength/MortgageLength.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+  import ButtonGroup from '../ButtonGroup';
+  import TextField from '../TextField';
+
+  let value;
+  $: currentValue = value;
+  const content = [
+    { display: 30, value: 30 },
+    { display: 20, value: 20 },
+    { display: 15, value: 15 },
+    { display: 10, value: 10 }
+  ];
+</script>
+
+<div class="mortgage-length">
+  <div class="mb-2">Mortgage Length</div>
+  <div class="flex">
+    <div class="mr-4">
+      <TextField options={{ value: currentValue }} />
+    </div>
+
+    <ButtonGroup {content} bind:inputValue={value} />
+  </div>
+</div>
