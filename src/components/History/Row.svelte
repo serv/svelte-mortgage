50b5db1f85a9faa7aa11587613011ac9a007d329
Jason Kim <iamjsonkim@gmail.com>
2021-04-08 09:46:36 -0700

    Rows

--- src/components/History/Row.svelte
+++ src/components/History/Row.svelte
@@ -6,33 +6,34 @@
   export let percentage;
   export let mortgageLength;
 
-  start = start ? start : false;
-  end = end ? end : false;
+  start = start ? start === 'true' : false;
+  end = end ? end === 'true' : false;
+
   homePrice = homePrice ? homePrice : '0';
   downPayment = downPayment ? downPayment : '0';
   percentage = percentage ? percentage : '0';
   mortgageLength = mortgageLength ? mortgageLength : '0';
 
-  let base = processClass(start, end);
+  let baseClass = processClass(start, end);
 
   function processClass(start, end) {
-    let base = 'flex flex-row max-w-max';
+    let baseClass = 'flex flex-row max-w-max';
 
     if (start && end) {
-      base += ' border';
+      baseClass += ' border';
     } else if (start) {
-      base += ' border-t border-r border-l';
+      baseClass += ' border-t border-r border-l';
     } else if (end) {
-      base += ' border-b border-r border-l';
+      baseClass += ' border-b border-r border-l';
     } else {
-      base += ' border-r border-l';
+      baseClass += ' border-r border-l';
     }
 
-    return base;
+    return baseClass;
   }
 </script>
 
-<div class={base}>
+<div class={baseClass}>
   <div class="w-40 border-r pl-2">
     $ {homePrice}
   </div>
---
+++ src/components/History/Rows.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+  import Row from './Row.svelte';
+
+  export let content;
+</script>
+
+{#each content as el, i}
+  {#if i == 0}
+    <Row
+      start="true"
+      end="true"
+      homePrice={el.homePrice}
+      downPayment={el.downPayment}
+      percentage={el.percentage}
+      mortgageLength={el.mortgageLength}
+    />
+  {:else}
+    <Row
+      start="false"
+      end="true"
+      homePrice={el.homePrice}
+      downPayment={el.downPayment}
+      percentage={el.percentage}
+      mortgageLength={el.mortgageLength}
+    />
+  {/if}
+{/each}
