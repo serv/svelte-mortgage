b6d803b58a207e4438b21d58f09b6db164eb2043
Jason Kim <iamjsonkim@gmail.com>
2021-04-08 00:36:22 -0700

    Row complete

--- src/components/History/Row.svelte
+++ src/components/History/Row.svelte
@@ -1,28 +1,48 @@
 <script lang="ts">
-  import TextField from '../TextField';
-
+  export let start;
+  export let end;
   export let homePrice;
   export let downPayment;
   export let percentage;
   export let mortgageLength;
 
+  start = start ? start : false;
+  end = end ? end : false;
   homePrice = homePrice ? homePrice : '0';
   downPayment = downPayment ? downPayment : '0';
   percentage = percentage ? percentage : '0';
   mortgageLength = mortgageLength ? mortgageLength : '0';
+
+  let base = processClass(start, end);
+
+  function processClass(start, end) {
+    let base = 'flex flex-row max-w-max';
+
+    if (start && end) {
+      base += ' border';
+    } else if (start) {
+      base += ' border-t border-r border-l';
+    } else if (end) {
+      base += ' border-b border-r border-l';
+    } else {
+      base += ' border-r border-l';
+    }
+
+    return base;
+  }
 </script>
 
-<div class="flex flex-row border max-w-max">
-  <div class="w-40">
-    {homePrice}
+<div class={base}>
+  <div class="w-40 border-r pl-2">
+    $ {homePrice}
   </div>
-  <div class="w-24">
-    {downPayment}
+  <div class="w-24 border-r pl-2">
+    {downPayment} %
   </div>
-  <div class="w-24">
-    {percentage}
+  <div class="w-24 border-r pl-2">
+    {percentage} %
   </div>
-  <div class="w-24">
+  <div class="w-24 pl-2">
     {mortgageLength}
   </div>
 </div>
--- src/stories/components/History/Row.stories.ts
+++ src/stories/components/History/Row.stories.ts
@@ -20,3 +20,43 @@ WithValues.args = {
   percentage: 2.725,
   mortgageLength: 30
 };
+
+export const WithStartEnd = Template.bind({});
+WithStartEnd.args = {
+  homePrice: 1_234_000,
+  downPayment: 25,
+  percentage: 2.725,
+  mortgageLength: 30,
+  start: true,
+  end: true
+};
+
+export const WithStart = Template.bind({});
+WithStart.args = {
+  homePrice: 1_234_000,
+  downPayment: 25,
+  percentage: 2.725,
+  mortgageLength: 30,
+  start: true,
+  end: false
+};
+
+export const WithEnd = Template.bind({});
+WithEnd.args = {
+  homePrice: 1_234_000,
+  downPayment: 25,
+  percentage: 2.725,
+  mortgageLength: 30,
+  start: false,
+  end: true
+};
+
+export const WithInBetween = Template.bind({});
+WithInBetween.args = {
+  homePrice: 1_234_000,
+  downPayment: 25,
+  percentage: 2.725,
+  mortgageLength: 30,
+  start: false,
+  end: true
+};
