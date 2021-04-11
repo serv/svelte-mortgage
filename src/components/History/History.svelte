56e9ce82abe45d4556c0beaf45c3050e73b2d4bd
Jason Kim <iamjsonkim@gmail.com>
2021-04-11 12:18:22 -0700

    Able to handle Reset properly

--- src/components/History/History.svelte
+++ src/components/History/History.svelte
@@ -3,22 +3,44 @@
   import { history } from '../../services/stores';
   import Button from '../Button';
 
+  let baseClass = 'max-h-64 mb-4';
+  $: {
+    if ($history.length > 0) {
+      baseClass = 'max-h-64 mb-4 overflow-y-scroll';
+    } else {
+      baseClass = 'max-h-64 mb-4';
+    }
+  }
+
   function handleReset() {
     history.reset();
-    console.log(localStorage.getItem('history'));
   }
 </script>
 
 <div class="mb-10">
-  <div class="max-h-64 overflow-y-scroll mb-4">
-    <div class="flex flex-row max-w-max border-l border-r border-t">
-      <div class="w-40 border-r pl-2">Home Price</div>
-      <div class="w-32 border-r pl-2">Down Payment</div>
-      <div class="w-32 border-r pl-2">Interest Rate</div>
-      <div class="w-36 pl-2">Mortgage Length</div>
-    </div>
+  <div class={baseClass}>
+    {#if $history.length > 0}
+      <div class="flex flex-row max-w-max border-l border-r border-t">
+        <div class="w-40 border-r pl-2">Home Price</div>
+        <div class="w-32 border-r pl-2">Down Payment</div>
+        <div class="w-32 border-r pl-2">Interest Rate</div>
+        <div class="w-36 pl-2">Mortgage Length</div>
+      </div>
+    {:else}
+      <div class="flex flex-row max-w-max border">
+        <div class="w-40 border-r pl-2">Home Price</div>
+        <div class="w-32 border-r pl-2">Down Payment</div>
+        <div class="w-32 border-r pl-2">Interest Rate</div>
+        <div class="w-36 pl-2">Mortgage Length</div>
+      </div>
+      <div class="flex w-full border-l border-r border-b justify-center py-6">
+        No result
+      </div>
+    {/if}
 
-    <Rows content={$history} />
+    {#if $history.length > 0}
+      <Rows content={$history} />
+    {/if}
   </div>
 
   <div class="flex justify-center">
