b3aaf8bd56ab9d1d78f230bf91a59d89332579bd
Jason Kim <iamjsonkim@gmail.com>
2021-04-10 18:46:33 -0700

    dashboard 1

--- src/components/History/History.svelte
+++ src/components/History/History.svelte
@@ -6,9 +6,9 @@
 <div class="max-h-64 overflow-y-scroll">
   <div class="flex flex-row max-w-max border-l border-r border-t">
     <div class="w-40 border-r pl-2">Home Price</div>
-    <div class="w-24 border-r pl-2">Down Payment</div>
-    <div class="w-24 border-r pl-2">Interest Rate</div>
-    <div class="w-24 pl-2">Mortgage Length</div>
+    <div class="w-32 border-r pl-2">Down Payment</div>
+    <div class="w-32 border-r pl-2">Interest Rate</div>
+    <div class="w-36 pl-2">Mortgage Length</div>
   </div>
 
   <Rows content={$history} />
