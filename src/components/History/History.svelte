863f8e998c5c7e5448905435fa9c7ac1f652d3ab
Jason Kim <iamjsonkim@gmail.com>
2021-05-11 09:16:35 -0700

    History: side bar looks good

--- src/components/History/History.svelte
+++ src/components/History/History.svelte
@@ -27,9 +27,9 @@
       <thead>
         <tr>
           <th class="px-4 py-2">Home Price</th>
-          <th class="px-4 py-2 md:block hidden">Down Payment</th>
-          <th class="px-4 py-2 md:block hidden">Interest Rate</th>
-          <th class="px-4 py-2 md:block hidden">Mortgage Length</th>
+          <th class="px-4 py-2 lg:block hidden">Down Payment</th>
+          <th class="px-4 py-2 lg:block hidden">Interest Rate</th>
+          <th class="px-4 py-2 lg:block hidden">Mortgage Length</th>
           <th class="px-4 py-2">Delete</th>
         </tr>
       </thead>
@@ -45,9 +45,9 @@
                 $ {row.homePrice}
               </div>
             </td>
-            <td class="border px-4 py-2 md:block hidden">{row.downPayment}</td>
-            <td class="border px-4 py-2 md:block hidden">{row.interestRate}</td>
-            <td class="border px-4 py-2 md:block hidden"
+            <td class="border px-4 py-2 lg:block hidden">{row.downPayment}</td>
+            <td class="border px-4 py-2 lg:block hidden">{row.interestRate}</td>
+            <td class="border px-4 py-2 lg:block hidden"
               >{row.mortgageLength}</td
             >
             <td class="border px-4 py-2">
