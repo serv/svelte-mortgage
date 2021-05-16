628e060672936d4eb79f4c612d9262207a7026cc
Jason Kim <iamjsonkim@gmail.com>
2021-05-16 15:47:18 -0700

    Google analytics

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,9 @@
 <script lang="ts">
   import Dashboard from './components/Dashboard';
+  import { GoogleAnalytics } from '@beyonk/svelte-google-analytics';
 </script>
 
+<GoogleAnalytics properties={['G-FDM18362XN']} />
 <Dashboard />
 
 <style global>
